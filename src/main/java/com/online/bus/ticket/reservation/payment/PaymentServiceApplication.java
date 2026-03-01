c8613ec0e698d27c0d1dcb81fc411f1f9f7b578a
Kiran Richard Johny D <kiranrichardjohny@gmail.com>
2026-03-01 17:32:32 +0530

    Eureka Client- Service discovery setup

--- src/main/java/com/online/bus/ticket/reservation/payment/PaymentServiceApplication.java
+++ src/main/java/com/online/bus/ticket/reservation/payment/PaymentServiceApplication.java
@@ -2,7 +2,9 @@ package com.online.bus.ticket.reservation.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PaymentServiceApplication {
 
